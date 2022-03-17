854ce2d5cf147b41448eee76f8eb00a74221ea02
Ramona Snider <rls2061@yahoo.com>
2022-03-16 20:06:34 -0700

    added one line of assignment

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ let monthlyExpenses = 0
 let annualExpenses = 
 
 window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt(weeklyExpenseQuestions[0])
 console.log(answer)
 // Your Code Here
